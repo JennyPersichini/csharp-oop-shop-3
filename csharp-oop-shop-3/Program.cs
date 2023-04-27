d94e876c4ef0dd5d2e298fc0d499b7c613099f8b
JennyPersichini <nenny1096@gmail.com>
2023-04-27 15:06:28 +0200

    prova

--- csharp-oop-shop-3/Program.cs
+++ csharp-oop-shop-3/Program.cs
@@ -1,2 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
+
+using csharp_oop_shop_2;
+
+//prodotto(nome, descrizione, prezzo, iva, litri, ph, sorgente)
+
+/*Acqua bottiglia = new Acqua("Vitasnella", "Acqua naturale", 0.5, 5, 1.5, 7, "vitas");
+
+bottiglia.StampaProdotto();
+bottiglia.Bevi(0.9);
+bottiglia.StampaProdotto();
+bottiglia.Riempi(0.4);
+bottiglia.StampaProdotto();
+bottiglia.Svuota();
+bottiglia.StampaProdotto();
+
+//prodotto(nome, descrizione, prezzo, iva, quantitaFrutta)
+
+SacchettoFrutta Sacco = new("Mele", "Sacchetto di mele verdi", 3.5, 10, 4);
+
+Sacco.StampaProdotto();
+Sacco.MettiNelSacchetto(6);
+Sacco.StampaProdotto();
+
+//prodotto(nome, descrizione, prezzo, iva, marca, colore, accesa = false)
+
+Elettrodomestico Lavatrice = new("Lavatrice", "Lava i panni", 260, 20, "Beko", "Bianca");
+
+Lavatrice.StampaProdotto();*/
+/*Lavatrice.Accenditi();*/
+/*Lavatrice.Spegniti();*/
